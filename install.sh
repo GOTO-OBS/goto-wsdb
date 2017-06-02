71d3f608d5187f2d99a53109df1fc3d96f5e3d8e
Andy Casey <andycasey@gmail.com>
2017-06-02 16:33:08 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,11 +5,13 @@ cd q3c/
 make
 sudo make install
 cd ../
+# TODO: Need PGVERSION if not on Travis...
 sudo chmod 777 /etc/postgresql/$PGVERSION/main/pg_hba.conf
 sudo echo "local   all         postgres                          trust" > /etc/postgresql/$PGVERSION/main/pg_hba.conf
 sudo echo "local   all         all                               trust" >> /etc/postgresql/$PGVERSION/main/pg_hba.conf
 sudo echo "host    all         all         127.0.0.1/32          trust" >> /etc/postgresql/$PGVERSION/main/pg_hba.conf
 sudo echo "host    all         all         ::1/128               trust" >> /etc/postgresql/$PGVERSION/main/pg_hba.conf
+sudo service postgresql restart
 psql -U postgres -c "CREATE USER goto"
 psql -U postgres -c "CREATE DATABASE wsdb"
 psql -U postgres -c "ALTER USER goto WITH PASSWORD '$GOTO_PASSWORD'"
