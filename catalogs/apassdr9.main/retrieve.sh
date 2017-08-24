27e259b8f971ba790464358b6ed025c68d7a2355
Andy Casey <andycasey@gmail.com>
2017-08-24 10:58:59 +1000

    update apass scripts

---
+++ catalogs/apassdr9.main/retrieve.sh
@@ -0,0 +1,4 @@
+wget -nc https://www.aavso.org/sites/default/files/apass/apass_dr9a.zip
+wget -nc https://www.aavso.org/sites/default/files/apass/apass_dr9b.zip
+unzip apass_dr9a.zip
+unzip apass_dr9b.zip
