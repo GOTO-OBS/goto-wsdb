78f0e059fe3f410cce06b7795ee4faeda83fae87
Andy Casey <andycasey@gmail.com>
2017-08-29 17:20:05 +1000

    add code examples for different queries

--- wsdb/wsdb.py
+++ wsdb/wsdb.py
@@ -1,66 +1,25 @@
+# -*- coding: utf-8 -*-
 
 import numpy as np
-import os
-import psycopg2 as pg
-import yaml
 from astropy.table import Table
-from time import time
 
+from .base import BaseWSDB
 
-class WSDB(object):
+__all__ = ["WSDB"]
 
-    def __init__(self, **kwargs):
+class WSDB(BaseWSDB):
+
+    """ A client to access the World Sky Database. """
+
+    def radial_query(self, catalogue_name, ra, dec, radius=1/3600.0, **kwargs):
         r"""
-        Create a new database connection to the World Sky database.
+        Query a catalogue in the World Sky Database for all objects within the 
+        radius of a specific location. This is equivalent to performing a cone
+        search.
 
-        The connection parameters can be specified as a string:
-
-            wsdb = WSDB("user=goto password=secret")
-
-        or using a set of keyword arguments:
-
-            wsdb = WSDB(user="goto", password="secret")
-
-        Or as a mix of both. The basic connection parameters are:
-
-        - *dbname*: the database name
-        - *database*: the database name (only as keyword argument)
-        - *user*: the user name used to authenticate
-        - *password*: password used to authenticate
-        - *host*: database host address (defaults to UNIX socket if not given)
-        - *port*: connection port number (defaults to 5432 if not given)
-        """
-
-        default_configuration_filename = os.path.expanduser("~/.wsdb.yaml")
-        if os.path.exists(default_configuration_filename):
-            with open(default_configuration_filename, "r") as fp:
-                default_configuration = yaml.load(fp)
-
-            for k, v in default_configuration.items():
-                kwargs.setdefault(k, v)
-
-        self._default_return_as_table = True
-        self._connection = pg.connect(**kwargs)
-        return None
-
-
-    @property
-    def catalogues(self):
-        r"""
-        Return a list of the astronomical catalogues available.
-        """
-
-        _, catalogue_names, __ = self.execute("""
-            SELECT table_name
-            FROM information_schema.tables
-            WHERE table_schema = 'public'""", fetch=True)
-        return tuple(*catalogue_names)
-
-
-    def cone_search(self, ra, dec, catalog, radius=1/3600.0, **kwargs):
-        r"""
-        Query a catalog in the World Sky Database for all objects within radius of
-        a specific location.
+        :param catalogue_name:
+            The source catalogue to query. Available catalogues are accessible
+            through the ``WSDB.catalogues`` attribute.
 
         :param ra:
             The right ascension of the location in degrees.
@@ -68,92 +27,156 @@ class WSDB(object):
         :param dec:
             The declination of the location in degrees.
 
-        :param catalog:
-            The source catalog to query.
-
         :param radius: [optional]
             The radius of the search cone in degrees.
         """
 
-        # TODO: check if ra/dec are an array
+        self._check_catalogue(catalogue_name)
 
+        # TODO: check if ra/dec are an array
+        if np.array(ra).size != np.array(dec).size \
+        or np.array(ra).size != 1:
+            raise ValueError("ra and dec must be single float-like values")
+
+        query = """
+            SELECT *
+            FROM {catalogue_name}
+            WHERE q3c_radial_query(ra, dec, {ra}, {dec}, {radius})
+            """.format(
+                catalogue_name=catalogue_name, ra=ra, dec=dec, radius=radius)
+
+        return self.select(query, **kwargs)
+
+
+    def polygon_query(self, catalogue_name, ras, decs, **kwargs):
+        r"""
+        Query a catalogue in the World Sky Database for all objects within the
+        region bounded by the polygon on the sphere. 
+
+        :param catalogue_name:
+            The source catalogue to query. Available catalogues are accessible
+            through the ``WSDB.catalogues`` attribute.
+
+        :param ras:
+            An array-like object of right ascensions for each polygon point in
+            degrees.
+
+        :param decs:
+            An array-like object of declinations for each polygon point in
+            degrees.        
+        """
+
+        self._check_catalogue(catalogue_name)
+
+        ras = np.atleast_1d(ras).flatten()
+        decs = np.atleast_1d(decs).flatten()
+
+        if ras.size != decs.size or ras.size < 3:
+            raise ValueError("ras and decs should be the same length "\
+                             "and greater than 3 ({} < 3)".format(ras.size))
+        
+        ps = np.vstack([ras, decs]).T.flatten()
+        query = """
+            SELECT *
+            FROM {0}
+            WHERE q3c_poly_query(ra, dec, '{{{1}}}');
+        """.format(catalogue_name, ", ".join(ps.astype(str)))
+
+        return self.select(query, **kwargs)
+
+
+    def ellipse_query(self, catalogue_name, ra, dec, major_axis, axis_ratio, 
+        positional_angle, **kwargs):
+        r"""
+        Query a catalogue in the World Sky Database for all objects within the
+        region bounded by an ellipse on the sphere.
+        
+        :param catalogue_name:
+            The source catalogue to query. Available catalogues are accessible
+            through the ``WSDB.catalogues`` attribute.
+
+        :param ra:
+            The right ascension of the ellipse centre in degrees.
+
+        :param dec:
+            The declination of the ellipse centre in degrees.
+
+        :param major_axis:
+            The major axis of the ellipse.
+
+        :param axis_ratio:
+            The axis ratio of the ellipse.
+
+        :param positional_angle:
+            The positional angle of the ellipse.
+        """
+
+        self._check_catalogue(catalogue_name)
+
+        # TODO: check values of ra, dec, major_axis, axis_ratio, positional_angle
 
         query = """
             SELECT *
             FROM %s
-            WHERE q3c_radial_query(ra, dec, %s, %s, %s)
-            """
-        values = (catalog, ra, dec, radius)
+            WHERE q3c_ellipse_query(ra, dec, %s, %s, %s, %s, %s);
+        """
+        values = (
+            catalogue_name, ra, dec, 
+            major_axis, axis_ratio, positional_angle
+        )
 
         return self.select(query, values, **kwargs)
 
 
-    def select(self, query, values=None, **kwargs):
+
+    def nearest_neighbour_query(self, catalogue_name, ras, decs, radius=1.0/3600,
+        only_neighbours=False, **kwargs):
         r"""
-        Perform a SQL selec
-        select some results.
-        """
-        
-        names, rows, cursor = self.execute(query, values, fetch=True)
-        if len(rows) == 0:
-            return None
+        Select the nearest neighbour from a catalogue in the World Sky Database
+        for each position provided.
 
-        if kwargs.get("as_table", self._default_return_as_table):
-            counted_names = Counter(names)
-            duplicates = [k for k, v in counted_names.items() if v > 1]
-        
-            use_prefixes = map(str, range(max(counted_names.values()))) \
-                if isinstance(prefixes, bool) else prefixes
+        :param catalogue_name:
+            The source catalogue to query. Available catalogues are accessible
+            through the ``WSDB.catalogues`` attribute.
 
-            # Put the prefixes and names in the right order & format 
-            prefixes = [
-                ([], [use_prefixes[names[:i].count(n)]])[n in duplicates] \
-                for i, n in enumerate(names)]
-            names = [[n] for n in names]
-            names = [".".join(p + n) for p, n in zip(prefixes, names)]
-            return Table(rows=rows, names=names)
+        :param ras:
+            An array-like object of right ascensions. These will be available in
+            the returned table as 'input_ra'.
 
-        else:
-            return rows
+        :param decs:
+            An array-like object of declinations. These will be available in the
+            returned table as 'input_dec'.
 
-
-    def execute(self, query, values=None, fetch=False, **kwargs):
-        r"""
-        Execute a SQL query on  the database.
-
-        :param query:
-            The SQL query to execute.
-
-        :param values: [optional]
-            Values to use when formatting the SQL string.
-
+        :param only_neighbours: [optional]
+            Only return rows with neighbours (default: False).
         """
 
-        t_init = time()
-        try:
-            with self._connection.cursor() as cursor:
-                cursor.execute(query, values)
-                results = cursor.fetchall() if fetch else None
+        self._check_catalogue(catalogue_name)
 
-        except pg.ProgrammingError:
-            logger.exception("SQL query failed: {} {}".format(query, values))
-            cursor.close()
-            raise
+        # Create a temporary table on the WSDB.
+        positions = Table(
+            data=(np.arange(len(ras)), ras, decs),
+            names=("input_index", "input_ra", "input_dec"))
 
+        query = """
+            SELECT t.*, ss.*
+            FROM tmp_nn AS t {on_prefix}
+            LATERAL (
+                SELECT s.*
+                FROM {catalogue_name} AS s
+                WHERE q3c_join(t.input_ra, t.input_dec, s.ra, s.dec, {radius})
+                ORDER BY q3c_dist(t.input_ra, t.input_dec, s.ra, s.dec)
+                ASC LIMIT 1
+                )
+            AS ss {on_suffix};
+            """
+
+        kwds = dict(catalogue_name=catalogue_name, radius=radius)
+        if only_neighbours:
+            kwds.update(on_prefix=", ", on_suffix="")
         else:
-            t_taken = 1e3 * (time() - t_init)
+            kwds.update(on_prefix="LEFT JOIN", on_suffix="ON true")
 
-        names = None if cursor.description is None \
-                     else tuple([col[0] for col in cursor.description])
+        query = query.format(**kwds)
 
-        return (names, results, cursor)
-
-
-
-
-    def polygon_search():
-        raise NotImplementedError
-
-
-    def nearest_neighbour():
-        raise NotImplementedError
+        return self._local_join(query, "tmp_nn", positions, **kwargs)
