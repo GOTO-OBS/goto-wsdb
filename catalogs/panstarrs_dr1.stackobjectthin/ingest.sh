b38152581a9fd13c91e61a4e1afb49ba78048c63
Andy Casey <andycasey@gmail.com>
2017-10-10 09:49:14 +1100

    bugs and add ps1 ingest script template

---
+++ catalogs/panstarrs_dr1.stackobjectthin/ingest.sh
@@ -0,0 +1,5 @@
+psql -U postgres -d wsdb -a -f panstarrs_dr1_stackobjectthin_schema
+#python ingest.py
+#psql -U postgres -d wsdb -h localhost -a -c "CREATE INDEX ON apassdr9_main (q3c_ang2ipix(ra, dec));"
+#psql -U postgres -d wsdb -h localhost -a -c "CLUSTER apassdr9_main_q3c_ang2ipix_idx ON apassdr9_main;"
+#psql -U postgres -d wsdb -h localhost -a -c "ANALYZE apassdr9_main;"
